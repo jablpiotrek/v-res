1dbd038966c7cb2a3d6ffccc328e27a0332ffc0f
Piotr Jablonski <jabl.piotrek@gmail.com>
2020-12-16 00:18:22 +0100

    add day component

---
+++ src/components/Day.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="day">
+    <span :class="`day__date ${showDate ? 'day__date--show' : ''}`">{{ formattedDate }}</span>
+    <div>
+      DAILY_RES
+    </div>
+  </div>
+</template>
+
+<script>
+import format from 'date-fns/format';
+
+export default {
+  name: 'Day',
+  props: {
+    date: {
+      required: true,
+      type: Date,
+    },
+    showDate: {
+      required: true,
+      type: Boolean,
+    },
+  },
+  computed: {
+    formattedDate() {
+      return format(this.date, 'EEEE, dd.MM.yyyy');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .day {
+    &__date {
+      display: none;
+      &--show {
+        display: block;
+      }
+    }
+  }
+</style>
